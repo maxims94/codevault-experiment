bc99a810cd5675c2acb97459c134739d414a6641
Maxim Schmidt <maxim.schmidt@tum.de>
2023-08-08 14:29:06 +0200

    form link

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
                 <Image src={solana_icon} alt="Solana" width={30} />
                 <h1 className="text-2xl ml-3">OpenSource</h1>
               </div>
-              <a href="https://airtable.com/appaWGnMf4eBJS9pb/shrvRbszXG6en0Ruf" target="_blank" className="text-xl hover:underline">Add Project</a>
+              <a href="https://airtable.com/appaWGnMf4eBJS9pb/shrLQ5YdZ55WAZ0uY" target="_blank" className="text-xl hover:underline">Add Project</a>
             </div>
             
             <h1 className="text-3xl mb-10 mt-5">Search for Open Source projects built on Solana!</h1>
